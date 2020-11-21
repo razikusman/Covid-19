f5486d7685ea41716f52c506c9c8f24fa18689c2
razikusman <razikusman@gmail.com>
2020-11-21 18:00:52 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { CovidService } from './covid.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Covi-dashboard';
+
+  datas : any = {};
+
+  constructor(private service : CovidService){
+    this.service.getdata().subscribe(data =>{
+      console.log(data);
+      this.datas = data})
+  }
+
+  title = 'Covid-dashboard';
 }
